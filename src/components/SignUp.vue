def66084c51f425a80f586a2e821fe715cb07dff
Rajasekhar <55192619+Rajasekhar1989@users.noreply.github.com>
2022-04-30 13:57:11 +0530

    Signup design updated.

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,29 +1,47 @@
 
 <template>
-<div class="flex_wrap" style="height:100vh">
-    <div class="login register">
-        <h4>SignUp</h4>        
+<div class="user_log">
+    <div class="main-wrap">
+      <div class="row h-100" >
+      <aside class="col-sm-6 login clearfix">
+        <div class="login-inner">
+          <figure>
+              <img :src="$store.state.themeLogo" alt="" />
+          </figure>
+        <h5>SignUp</h5>
         <div class="form-group">
-          <input type="text" class="form-control formInput" v-model="SignUpName" />
           <label for="name" class="formLabel">Name</label>
+          <input type="text" class="form-control formInput" v-model="SignUpName" />
         </div>
-        <div class="form-group">
-          <input type="text" class="form-control formInput" v-model="SignUpEmail" />
+        <div class="form-group">          
           <label for="name" class="formLabel">Email</label>
+          <input type="text" class="form-control formInput" v-model="SignUpEmail" />
         </div>
-        <div class="form-group">
-          <input type="password" class="form-control formInput" v-model="SignUpPassword"/>
+        <div class="form-group">          
           <label for="name" class="formLabel">Password</label>
+          <input type="password" class="form-control formInput" v-model="SignUpPassword"/>
         </div>
-        <div class="formSubmit">
-          <button v-on:click="signUp" class="btn btn-info float-end">Sign Up</button>
-          <span>Click here to <router-link to="/login">Login</router-link></span>
+        <div class="form-group formSubmit d-flex justify-content-between align-items-center">          
+          <span>Already a user? <router-link to="/login">LOGIN</router-link></span>
+          <button v-on:click="signUp" class="btn btn-primary"><box-icon name='log-out' ></box-icon> Sign Up</button>
         </div>
+        </div>
+      </aside>
+      <aside class="login-info col-sm-6">
+        <div class="login-info-inner">
+          <h3>Welcome to <span>Dashboard Theme</span></h3>
+          <p>Sign Up To Create Account</p>
+
+        </div>
+      </aside>
+      </div>
     </div>
 </div>
 </template>
 <script>
+
 import axios from "axios";
+import $ from 'jquery';
 export default {
     name:'SignUp',
     data() {
@@ -32,6 +50,21 @@ export default {
         SignUpEmail: "",
         SignUpPassword: "",        
       }
+    },
+    mounted(){
+      $('input, .form-group').focus(function(){
+        $(this).parents('.form-group').addClass('focused');
+      });
+  
+      $('input').blur(function(){
+        var inputValue = $(this).val();
+        if ( inputValue == "" ) {
+          $(this).removeClass('filled');
+          $(this).parents('.form-group').removeClass('focused');  
+        } else {
+          $(this).addClass('filled');
+        }
+      })
     },
     methods:{
     async signUp() {
